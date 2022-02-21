966ebb249bf562bf9f078e8bb7f95a9bab01d89f
wuling <wuling@kylinos.com.cn>
2022-02-21 10:43:46 +0800

    first commit

---
+++ contract.py
@@ -0,0 +1,43 @@
+import datetime as dt
+from requests import HTTPError
+import eospy.cleos
+import eospy.keys
+import pytz
+from logger import log
+from encrypt import read_file, prpcrypt
+from settings import user_param, cfg
+
+
+def push_transaction(params_json):
+    # this url is to a testnet that may or may not be working.
+    # We suggest using a different testnet such as kylin or jungle
+    #
+    ce = eospy.cleos.Cleos(url=user_param.rpc_domain)
+    payloads = []
+    for item in params_json['actions']:
+        arguments = item['data']
+        payload = {
+            "account": item['account'],
+            "name": item['name'],
+            "authorization": item['authorization'],
+        }
+
+        # Converting payload to binary
+        data = ce.abi_json_to_bin(payload['account'], payload['name'], arguments)
+        # Inserting payload binary form as "data" field in original payload
+        payload['data'] = data['binargs']
+        payloads.append(payload)
+    # final transaction formed
+    trx = {"actions": payloads}
+    trx['expiration'] = str(
+        (dt.datetime.utcnow() + dt.timedelta(seconds=60)).replace(tzinfo=pytz.UTC))
+    # use a string or EOSKey for push_transaction
+    # key = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+    # use EOSKey:
+    
+    key = eospy.keys.EOSKey(user_param.private_key)
+    try:
+        resp = ce.push_transaction(trx, key, broadcast=True)
+        return True, resp
+    except HTTPError as e:
+        return False, str(e)
